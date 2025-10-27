d9974c1d0fc06d7fa7d78449173de0333ba7cdc1
nohawaheed <nohawaheed.1991@gmail.com>
2025-10-27 22:17:07 +0300

    feat: add reverseWords implementation

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -370,8 +370,9 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const words = str.split(' ');
+  return words.map((value) => value.split('').reverse().join('')).join(' ');
 }
 
 /**
