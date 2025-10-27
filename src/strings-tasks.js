2a95fc95938e00b75ce47ceedc9299433305ffff
nohawaheed <nohawaheed.1991@gmail.com>
2025-10-27 20:57:43 +0300

    feat: add countVowels implementation

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -311,8 +311,10 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowelsRegex = /[aeiouyAEIOUY]/g;
+  const matchedCount = str.match(vowelsRegex);
+  return matchedCount?.length ?? 0;
 }
 
 /**
