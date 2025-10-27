ef6f88c840cb75f04a3b3fde1194f4309ffab7f3
nohawaheed <nohawaheed.1991@gmail.com>
2025-10-27 22:06:39 +0300

    feat: add findLongestWord implementation

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -349,8 +349,15 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const words = sentence.split(' ');
+  let longestWrord = '';
+  for (let i = 0; i < words.length; i += 1) {
+    if (words[i].length > longestWrord.length) {
+      longestWrord = words[i];
+    }
+  }
+  return longestWrord;
 }
 
 /**
